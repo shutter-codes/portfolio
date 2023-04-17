5575a6985adb98ee6f3208fd1923e90b13882f3e
Naitik Sharma <snaitik0238@gmail.com>
2023-04-17 23:23:45 +0530

    resume added

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -6,9 +6,9 @@ import pdf from "../../Assets/../Assets/naitikResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-const resumeLink = ""
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+const resumeLink =
+  "https://github.com/shutter-codes/portfolio/blob/master/src/Assets/naitikResume.pdf";
   function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
@@ -33,7 +33,7 @@ const resumeLink = ""
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
