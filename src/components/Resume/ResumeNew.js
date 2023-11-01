3a4691f540ed5471955f3cbaff406467d6a808e3
Naitik Sharma <126913992+shutter-codes@users.noreply.github.com>
2023-11-02 02:11:54 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/naitikResume.pdf";
+import pdf from "../../Assets/../Assets/NaitikSharma.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 const resumeLink =
-  "https://github.com/shutter-codes/portfolio/blob/master/src/Assets/naitikResume.pdf";
+  "https://github.com/shutter-codes/portfolio/blob/master/src/Assets/NaitikSharma.pdf";
   function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
